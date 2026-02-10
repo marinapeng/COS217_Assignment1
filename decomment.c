224be5daac40c3bc639de19092b1e7221731aa15
Marina Peng <mp3262@prineton.edu>
2026-02-10 10:32:33 -0500

    fixed bug with returns state variable in each method

--- decomment.c
+++ decomment.c
@@ -10,157 +10,134 @@ input character. Writes c to stdout if appropriate. Returns
 the next DFA state. */
 enum Statetype
 handleNormalState (int c) {
-    enum Statetype state;
     if (c == '/') {
-        state = SLASH;
+        return SLASH;
     }
     else if (c == '"') {
         putchar (c);
-        state = IN_STRING;
+        return IN_STRING;
     }
     else if (c == '\'') {
         putchar (c);
-        state = IN_CHAR;
+        return IN_CHAR;
     }
     putchar(c);
-    state = NORMAL;
-    return state;
+    return NORMAL;
 }
 
 /*Implements the SLASH state of the DFA. c is the current
 input character. A '/' has already been read and is being held. 
 Determines whether a comment begins or '/' should be printed.
 Writes appropriate output to stdout. Returns the next DFA state.*/
-enum Statetype
-handleSlashState (int c) {
-    enum Statetype state;
+enum Statetype handleSlashState (int c) {
     if (c == '*') {
         putchar (' '); /*Beginning of a comment: replace with a single space*/
-        state = IN_COMMENT;
+        return IN_COMMENT;
     }
     else if (c == '/') {
         putchar (c); /*Prints previous '/', holds this '/'.*/
-        state = SLASH;
+        return SLASH;
     }
     else if (c == '"') {
         putchar ('/');
         putchar (c);
-        state = IN_STRING;
+        return IN_STRING;
     }
     else if (c == '\'') {
         putchar ('/');
         putchar (c);
-        state = IN_CHAR;
+        return IN_CHAR;
     }
     putchar ('/');
     putchar (c);
-    state = NORMAL;
-    return state;
+    return NORMAL;
 }
 
 /*Implements the IN_COMMENT state of the DFA. c is the current
 input character inside a block comment. Newlines are written to
 stdout to preserve line numbering. All other characters are discarded.
 Returns the next DFA state.*/
-enum Statetype
-handleInCommentState (int c) {
-    enum Statetype state;
+enum Statetype handleInCommentState (int c) {
     if (c == '*') {
-        state = COMMENT_STAR;
+        return COMMENT_STAR;
     }
     else if (c == '\n') {
         putchar (c);
-        state = IN_COMMENT;
+        return IN_COMMENT;
     }
-    state = IN_COMMENT;
-    return state;
+    return IN_COMMENT;
 }
 
 /*/*Implements the COMMENT_STAR state of the DFA. c is the current
 input character after a '*' has been seen inside a block comment.
 Determines whether the comment ends. Preserves new lines. Returns
 the next DFA state.*/
-enum Statetype
-handleCommentStarState (int c) {
-    enum Statetype state;
+enum Statetype handleCommentStarState (int c) {
     if (c == '/') {
-        state = NORMAL;
+        return NORMAL;
     }
     else if (c == '*') {
-        state = COMMENT_STAR;
+        return COMMENT_STAR;
     }
     else if (c == '\n') {
         putchar (c);
-        state = IN_COMMENT;
+        return IN_COMMENT;
     }
-    state = IN_COMMENT;
-    return state;
+    return IN_COMMENT;
 }
 
 /*Implements the IN_STRING state of the DFA. c is the
 current character inside a string literal. Handeles
 excaped characters and closing quotes. Writes output
 to stdout. Returns the next DFA state.*/
-enum Statetype
-handleInStringState (int c) {
-    enum Statetype state;
+enum Statetype handleInStringState (int c) {
     if (c == '"') {
         putchar (c);
-        state = NORMAL;
+        return NORMAL;
     }
     else if (c == '\\') {
         putchar (c);
-        state = STRING_ESC;
+        return STRING_ESC;
     }
     putchar (c);
-    state = IN_STRING;
-    return state;
+    return IN_STRING;
 }
 
 /*Implements the STRING_ESC state of the DFA. 
 c are the escaped characters inside a string
 literal. Writes c to stdout and returns to IN_STRING.*/
-enum Statetype
-handleStringEscState (int c) {
-    enum Statetype state;
+enum Statetype handleStringEscState (int c) {
     putchar (c);
-    state = IN_STRING;
-    return state;
+    return IN_STRING;
 }
 
 /*Implements the IN_CHAR state of the DFA. c is the
 current character inside a char literal. Handeles
 excaped characters and closing quotes. Writes output
 to stdout. Returns the next DFA state.*/
-enum Statetype
-handleInCharState (int c) {
-    enum Statetype state;
+enum Statetype handleInCharState (int c) {
     if (c == '"') {
         putchar (c);
-        state = NORMAL;
+        return NORMAL;
     }
     else if (c == '\\') {
         putchar (c);
-        state = CHAR_ESC;
+        return CHAR_ESC;
     }
     else if (c == '\n') {
         putchar (c);
-        state = IN_CHAR;
+        return IN_CHAR;
     }
     putchar (c);
-    state = IN_CHAR;
-    return state;
+    return IN_CHAR;
 }
 
 /*Implements the CHAR_ESC state of the DFA. 
 c are the escaped characters inside a char literal
 literal. Writes c to stdout and returns to IN_CHAR.*/
-enum Statetype
-handleCharEscState (int c) {
-    enum Statetype state;
+enum Statetype handleCharEscState (int c) {
     putchar (c);
-    state = IN_CHAR;
-    return state;
+    return IN_CHAR;
 }
 
 /*Determines whether the DFA state is accepting or
